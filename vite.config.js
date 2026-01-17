063c399289a5323e6693fe77cf3081139f6d292f
d4li3n <danielthe@cyberdude.com>
2026-01-17 17:17:09 +0100

    Img_fix

--- vite.config.js
+++ vite.config.js
@@ -27,8 +27,8 @@ export default defineConfig({
         start_url: "/",
 		    scope: "/",
         icons: [
-          { src: '/x.png', sizes: '192x192', type: 'image/png' },
-          { src: '/x2.png', sizes: '512x512', type: 'image/png' }
+          { src: '/web-app-manifest-192x192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/web-app-manifest-512x512.png', sizes: '512x512', type: 'image/png' }
         ],
         screenshots: [
           {
